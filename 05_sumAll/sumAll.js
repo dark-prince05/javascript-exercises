dc84816a7b82eaedcfb4081affca93627445d0fb
Dark-prince <barathraj0504@gmail.com>
2024-08-27 05:46:27 +0530

    Finished sumAll but not in a efficient way

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let res = 0;
+  if (start < 0 || end < 0) return "ERROR";
+  if (start % 1 != 0 || end % 1 != 0) return "ERROR";
+  if (Number(start) !== start || Number(end) !== end) return "ERROR";
 
+  if (start < end) {
+    for (let i = start; i <= end; i++) {
+      res += i;
+    }
+  } else {
+    for (let j = start; j >= end; j--) {
+      res += j;
+    }
+  }
+  return res;
 };
 
 // Do not edit below this line
